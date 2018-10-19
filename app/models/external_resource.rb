33daf660ca4aaa6a78d60e29f890d4212141ddf1
zeecho <zeecho@users.noreply.github.com>
2018-10-19 13:32:00 +0200

    Fix basic validation

--- app/models/external_resource.rb
+++ app/models/external_resource.rb
@@ -1,6 +1,6 @@
 class ExternalResource < ApplicationRecord
-  validate_presence_of :name
-  validate_presence_of :description
+  validates_presence_of :name
+  validates_presence_of :description
   validates :link, :format => URI::regexp(%w(http https))
   validates :img, :format => URI::regexp(%w(http https))
 end
