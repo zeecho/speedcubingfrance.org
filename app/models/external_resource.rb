a9e0cc5106cfc4b6383697f9193bffd570b03384
Philippe Virouleau <philippe.44@gmail.com>
2018-10-26 15:16:20 +0200

    Cleanup rank update and select generation

--- app/models/external_resource.rb
+++ app/models/external_resource.rb
@@ -3,18 +3,16 @@ class ExternalResource < ApplicationRecord
   validates_presence_of :description
   validates :link, :format => URI::regexp(%w(http https))
   validates :img, :format => URI::regexp(%w(http https))
+  validates_numericality_of :rank, only_integer: true, greater_than: 0, less_than_or_equal_to: ->(_) { ExternalResource.count + 1 }
 
   before_save :set_ranks_on_save, if: :will_save_change_to_rank?
   private def set_ranks_on_save
-    if self.rank_was.nil?
-      ExternalResource.where("rank >= ?", rank).update_all("rank = rank + 1")
+    previous_rank = self.rank_was || ExternalResource.count + 1
+    if self.rank > previous_rank
+      self.rank -= 1
+      ExternalResource.where("rank > ? and rank <= ?", previous_rank, self.rank).update_all("rank = rank - 1")
     else
-      if self.rank > self.rank_was
-	self.rank = self.rank - 1
-        ExternalResource.where("rank > ?", rank_was).where("rank <= ?", rank).update_all("rank = rank - 1")
-      else
-        ExternalResource.where("rank < ?", rank_was).where("rank >= ?", rank).update_all("rank = rank + 1")
-      end
+      ExternalResource.where("rank >= ? and rank < ?", self.rank, previous_rank).update_all("rank = rank + 1")
     end
   end
 
