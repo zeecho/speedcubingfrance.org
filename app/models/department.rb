b96f0def4a1172219701df84b43494f4f28acc53
zeecho <zeecho@users.noreply.github.com>
2019-01-03 23:55:26 +0100

    Import regions and departments during migration

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,12 @@
 class Department < ApplicationRecord
+  FRENCH_DEPARTMENTS_JSON_PATH = Rails.root.to_s + "/data/departments.json"
+  FRENCH_DEPARTMENTS = JSON.parse(File.read(FRENCH_DEPARTMENTS_JSON_PATH)).freeze
+  ALL_DEPARTMENTS = [
+    FRENCH_DEPARTMENTS.map do |dep|
+      { code: dep["code"], name: dep["name"], region: Region.find_by(code: dep["region_code"]) }
+    end
+  ].flatten.map { |d| Department.new(d) }.freeze
+
   belongs_to :region
   has_many :clubs
 
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,12 @@
 class Region < ApplicationRecord
+  FRENCH_REGIONS_JSON_PATH = Rails.root.to_s + "/data/regions.json"
+  FRENCH_REGIONS = JSON.parse(File.read(FRENCH_REGIONS_JSON_PATH)).freeze
+  ALL_REGIONS = [
+    FRENCH_REGIONS.map do |region|
+      { code: region["code"], name: region["name"] }
+    end
+  ].flatten.map { |r| Region.new(r) }.freeze
+
   has_many :departments
   validates_presence_of :code
   validates_presence_of :name
