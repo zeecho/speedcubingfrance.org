0fca6f9f2df70643dab317ca3b3eff612e1b125a
zeecho <zeecho@users.noreply.github.com>
2019-01-03 01:21:50 +0100

    Clean useless scaffold files

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,4 +1,3 @@
 class Club < ApplicationRecord
-  include HasOwners
   belongs_to :department
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,4 @@
 class Region < ApplicationRecord
-  include HasOwners
   has_many :departments
   validates_presence_of :code
   validates_presence_of :name
