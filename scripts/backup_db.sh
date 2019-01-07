ef3af448325ed3cec6d38bfff1ca1ef8b3d6b172
zeecho <zeecho@users.noreply.github.com>
2019-01-08 00:33:44 +0100

    Create backup script

---
+++ scripts/backup_db.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DESTINATION_DIR="/home/afs/backups"
+# create directory if it doesn't exist
+mkdir -p ${DESTINATION_DIR}
+
+# get the database password
+eval $(grep "DATABASE_PASSWORD=" /home/afs/speedcubingfrance.org/.env.production)
+
+# create a file which pg_dump will read (because there is no option to give it to the command line)
+echo "localhost:*:speedcubingfrance-prod:speedcubingfrance:${DATABASE_PASSWORD}" > .pgpass
+# it works only with this rights
+chmod 600 .pgpass
+
+FILENAME="${DESTINATION_DIR}/prod-$(date +%Y%m%d).dump"
+pg_dump -Fc --no-acl --no-owner -h localhost -U speedcubingfrance speedcubingfrance-prod > ${FILENAME}
+
+cp ${FILENAME} /tmp/prod.dump
+/home/afs/speedcubingfrance.org/bin/rails backup:send_backup_to_admins
+
+# no need to keep this file
+rm .pgpass
