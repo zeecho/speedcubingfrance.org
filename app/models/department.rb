a16e6126e57e4df278f2b5e636055d631d9da1ff
Antoine Piau <apiau@mediastay.com>
2019-03-29 12:02:41 +0100

    Change creation order region/dep/club

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,11 @@
 class Department < ApplicationRecord
+  belongs_to :region
+  has_many :clubs
+
+  validates_presence_of :code, allow_blank: false
+  validates_presence_of :name, allow_blank: false
+  validates_presence_of :region, allow_blank: false
+
   FRENCH_DEPARTMENTS_JSON_PATH = Rails.root.to_s + "/data/departments.json"
   FRENCH_DEPARTMENTS = JSON.parse(File.read(FRENCH_DEPARTMENTS_JSON_PATH)).freeze
   ALL_DEPARTMENTS = [
@@ -6,11 +13,4 @@ class Department < ApplicationRecord
       { code: dep["code"], name: dep["name"], region: Region.find_by(code: dep["region_code"]) }
     end
   ].flatten.map { |d| Department.new(d) }.freeze
-
-  belongs_to :region
-  has_many :clubs
-
-  validates_presence_of :code, allow_blank: false
-  validates_presence_of :name, allow_blank: false
-  validates_presence_of :region, allow_blank: false
 end
