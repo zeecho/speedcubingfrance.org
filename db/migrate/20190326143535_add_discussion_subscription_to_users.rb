2048b95a05ab46ded374967ef6dd95ac5be1425e
Antoine Piau <apiau@mediastay.com>
2019-04-02 15:11:56 +0200

    Merge migrations

--- db/migrate/20190326143535_add_discussion_subscription_to_users.rb
+++ db/migrate/20190326143535_add_discussion_subscription_to_users.rb
@@ -1,5 +1,6 @@
 class AddDiscussionSubscriptionToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :discussion_subscription, :boolean
+    add_column :users, :newsletter_subscription, :boolean
   end
 end
--- db/migrate/20190402113930_add_newsletter_subscription_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNewsletterSubscriptionToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :newsletter_subscription, :boolean
-  end
-end
