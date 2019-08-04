a291d7c9bcc7531d6fe82ea616cbe3d61e0a3f8a
Philippe Virouleau <philippe.44@gmail.com>
2019-08-04 16:27:45 +0200

    Transliterate subscriber name
    Fixes #75

--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -1,7 +1,7 @@
 module CompetitionsHelper
   def class_and_status_from_person(sub_by_id, sub_by_name, person)
     if @subscribers_by_id.include?(person["wcaId"]) ||
-       @subscribers_by_name.include?(person["name"].downcase)
+       @subscribers_by_name.include?(I18n.transliterate(person["name"].downcase))
       ["success", "Pris en charge par l'AFS"]
     elsif person["wcaId"]
       ["danger", "Doit payer"]
