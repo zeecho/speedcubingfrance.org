fc73cfd468465119247e6b42c3adcf3c34bf0b4c
zeecho <zeecho@users.noreply.github.com>
2019-12-01 22:59:46 +0100

    Make cachable work

--- app/models/club.rb
+++ app/models/club.rb
@@ -6,4 +6,8 @@ class Club < ApplicationRecord
   validates_presence_of :description
   validates :website, :format => URI::regexp(%w(http https)), :allow_blank => true
   validates :logo, :format => URI::regexp(%w(http https)), :allow_blank => true
+
+  def department
+    Department.c_find!(department_id)
+  end
 end
