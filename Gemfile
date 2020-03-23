4c4846bf5359b0a6dd06680876477695625d2172
Philippe Virouleau <philippe.44@gmail.com>
2020-03-23 18:33:49 +0100

    Update to rails 5.2

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ ruby '2.5.0'
 source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '5.1.6.2'
+gem 'rails', '5.2.4.2'
 gem 'rails-i18n', '~> 5.1'
 gem 'i18n-country-translations'
 gem 'webpacker', '~> 4.x'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
-
-# All js/images/css are handled through webpack
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.enabled = false
 
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
 # Cache static assets
+
 Rails.application.config.serve_static_assets = true
